9046900050e2a4db9f081ed71c7cfd1aba9d0335
Косарев Алексей <kosarev.aleksey7@wildberries.work>
2023-12-14 19:04:42 +0300

    2

--- backend/e2e_test/post_e2e_test.go
+++ backend/e2e_test/post_e2e_test.go
@@ -1,191 +1,60 @@
-//go:build e2e
-
 package e2e
 
 import (
 	"context"
-	json2 "encoding/json"
-	"errors"
-	"fmt"
 	"github.com/ozontech/cute"
-	"github.com/ozontech/cute/asserts/json"
-	"io"
 	"net/http"
 	"testing"
 	"time"
 )
 
 type RegisterRequest struct {
-	Login       string
-	Password    string
-	Picture     string
-	Description string
-	Mail        string
+	Login    string `json:"login"`
+	Password string `json:"password"`
 }
 
 type PublishRequest struct {
-	Content string
-	Perms   bool
+	GpName string `json:"gp_name"`
 }
 
-type ViewPost struct {
-	Content string `json:"content"`
-	Perms   bool   `json:"perms"`
-}
-
-type ViewPostsResponse struct {
-	Posts []ViewPost `json:"posts"`
-}
-
-type AssertBody func(body []byte) error
-
-func customAssertBody() AssertBody {
-	return func(bytes []byte) error {
-		if len(bytes) == 0 {
-			return errors.New("response body is empty")
-		}
-
-		var v ViewPostsResponse
-
-		err := json2.Unmarshal(bytes, &v)
-		if err != nil {
-			return fmt.Errorf("json unmarshal: %w", err)
-		}
-
-		if len(v.Posts) != 1 {
-			return errors.New("wrong response len")
-		}
-
-		if v.Posts[0].Content != "aaa" {
-			return errors.New("wrong response content")
-		}
-
-		if v.Posts[0].Perms != false {
-			return errors.New("wrong response perms")
-		}
-
-		return nil
-	}
-}
-
-func Test_ViewPosts(t *testing.T) {
-	tokens := make(map[string][]string)
-
+func Test_Drivers(t *testing.T) {
 	cute.NewTestBuilder().
-		Title("Просмотр платных и бесплатных постов").
-		Tags("posts").
+		Title("1 этап").
+		Tags("driver").
 		// Подготавливаем запрос на создание
-		CreateStep("Register user1").
+		CreateStep("1 этап").
 		RequestBuilder( // Создаём HTTP-запрос, который будет отправлен
-			cute.WithURI("http://localhost:8090/register"),
+			cute.WithURI("http://localhost:8080/auth/login"),
 			cute.WithMethod(http.MethodPost),
 			cute.WithMarshalBody(RegisterRequest{
-				Login:       "user1",
-				Password:    "password",
-				Picture:     "aaa",
-				Description: "aaa",
-				Mail:        "a@a.a",
+				Login:    "admin",
+				Password: "admin",
 			}),
-		).
-		ExpectExecuteTimeout(10*time.Second).
-		ExpectStatus(http.StatusCreated).
-		AssertBody(
-			json.Present("$.token"),
-		).
-		NextTest().
-		AfterTestExecute(
-			func(response *http.Response, errors []error) error {
-				b, err := io.ReadAll(response.Body)
-				if err != nil {
-					return err
-				}
-
-				temp, err := json.GetValueFromJSON(b, "$.token") // Получаем email из тела ответа
-				if err != nil {
-					return err
-				}
-
-				tokens["User-Token"] = []string{fmt.Sprint(temp)} // Сохраняем email
-
-				return nil
-			},
-		).
-		// Подготавливаем запрос на удаление
-		CreateStep("Publish free post").
-		RequestBuilder(
-			cute.WithURI("http://localhost:8090/posts"),
-			cute.WithMethod(http.MethodPost),
-			cute.WithHeaders(tokens),
-			cute.WithMarshalBody(PublishRequest{
-				Content: "aaa",
-				Perms:   false,
-			}),
-		).
-		AssertBody(
-			json.Equal("$.post.content", "aaa"),
-			json.Equal("$.published", true),
-		).
-		NextTest().
-		CreateStep("Publish paid post").
-		RequestBuilder(
-			cute.WithURI("http://localhost:8090/posts"),
-			cute.WithMethod(http.MethodPost),
-			cute.WithHeaders(tokens),
-			cute.WithMarshalBody(PublishRequest{
-				Content: "bbb",
-				Perms:   true,
-			}),
-		).
-		AssertBody(
-			json.Equal("$.post.content", "bbb"),
-			json.Equal("$.published", true),
-		).
-		NextTest().
-		CreateStep("Register user2").
-		RequestBuilder( // Создаём HTTP-запрос, который будет отправлен
-			cute.WithURI("http://localhost:8090/register"),
-			cute.WithMethod(http.MethodPost),
-			cute.WithMarshalBody(RegisterRequest{
-				Login:       "user2",
-				Password:    "password",
-				Picture:     "bbb",
-				Description: "bbb",
-				Mail:        "b@b.b",
-			}),
-		).
-		ExpectExecuteTimeout(10*time.Second).
-		ExpectStatus(http.StatusCreated).
-		AssertBody(
-			json.Present("$.token"),
-		).
-		NextTest().
-		AfterTestExecute(
-			func(response *http.Response, errors []error) error {
-				b, err := io.ReadAll(response.Body)
-				if err != nil {
-					return err
-				}
-
-				temp, err := json.GetValueFromJSON(b, "$.token") // Получаем email из тела ответа
-				if err != nil {
-					return err
-				}
-
-				tokens["User-Token"] = []string{fmt.Sprint(temp)} // Сохраняем email
-
-				return nil
-			},
-		).
-		CreateStep("View user1 profile posts").
-		RequestBuilder( // Создаём HTTP-запрос, который будет отправлен
-			cute.WithURI("http://localhost:8090/users/user1/posts"),
-			cute.WithMethod(http.MethodGet),
-			cute.WithHeaders(tokens),
 		).
 		ExpectExecuteTimeout(10*time.Second).
 		ExpectStatus(http.StatusOK).
-		AssertBody(
-			cute.AssertBody(customAssertBody()),
+		NextTest().
+		CreateStep("Get drivers").
+		RequestBuilder(
+			cute.WithURI("http://localhost:8080/api/drivers"),
+			cute.WithMethod(http.MethodGet),
 		).
+		NextTest().
+		CreateStep("change name").
+		RequestBuilder(
+			cute.WithURI("http://localhost:8080/api/grandprix/100/name"),
+			cute.WithMethod(http.MethodPatch),
+			cute.WithMarshalBody(PublishRequest{
+				GpName: "bbb",
+			}),
+		).
+		NextTest().
+		CreateStep("Get name").
+		RequestBuilder( // Создаём HTTP-запрос, который будет отправлен
+			cute.WithURI("http://localhost:8080/api/grandprix/100"),
+			cute.WithMethod(http.MethodGet),
+		).
+		ExpectExecuteTimeout(10*time.Second).
+		ExpectStatus(http.StatusOK).
 		ExecuteTest(context.Background(), t)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -7,13 +7,11 @@ require (
 	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.1
 	github.com/jmoiron/sqlx v1.3.5
-	github.com/labstack/echo/v4 v4.11.3
 	github.com/lib/pq v1.10.9
 	github.com/ozontech/allure-go/pkg/framework v0.6.29
+	github.com/ozontech/cute v0.1.14
 	github.com/stretchr/testify v1.8.4
 	github.com/swaggo/http-swagger/v2 v2.0.2
-	gorm.io/driver/postgres v1.5.4
-	gorm.io/gorm v1.25.5
 )
 
 require (
@@ -25,31 +23,22 @@ require (
 	github.com/go-openapi/spec v0.20.6 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.4.3 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josephburnett/jd v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/ohler55/ojg v1.12.9 // indirect
 	github.com/ozontech/allure-go/pkg/allure v0.6.12 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/swaggo/swag v1.8.1 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/mod v0.9.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	golang.org/x/tools v0.6.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	moul.io/http2curl/v2 v2.3.0 // indirect
 )
