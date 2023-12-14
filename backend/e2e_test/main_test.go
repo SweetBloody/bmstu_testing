81b84b143340f4cc1204bbda587e2b97cf2911f1
Косарев Алексей <kosarev.aleksey7@wildberries.work>
2023-12-14 18:29:44 +0300

    2

--- backend/e2e_test/main_test.go
+++ backend/e2e_test/main_test.go
@@ -7,61 +7,56 @@ import (
 	"flag"
 	"fmt"
 	"git.iu7.bmstu.ru/keo20u511/ppo/backend/internal/app/http"
-	"github.com/pressly/goose"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 	"os"
 	"testing"
 	"text/template"
 	"time"
 )
 
-func setupTestDatabase() (testcontainers.Container, string, int, error) {
-	containerReq := testcontainers.ContainerRequest{
-		Image:        "postgres:latest",
-		ExposedPorts: []string{"5432/tcp"},
-		WaitingFor:   wait.ForListeningPort("5432/tcp"),
-		Env: map[string]string{
-			"POSTGRES_DB":       "testdb",
-			"POSTGRES_PASSWORD": "postgres",
-			"POSTGRES_USER":     "postgres",
-		},
-	}
+//func setupTestDatabase() (testcontainers.Container, string, int, error) {
+//	containerReq := testcontainers.ContainerRequest{
+//		Image:        "postgres:latest",
+//		ExposedPorts: []string{"5432/tcp"},
+//		WaitingFor:   wait.ForListeningPort("5432/tcp"),
+//		Env: map[string]string{
+//			"POSTGRES_DB":       "testdb",
+//			"POSTGRES_PASSWORD": "postgres",
+//			"POSTGRES_USER":     "postgres",
+//		},
+//	}
+//
+//	dbContainer, _ := testcontainers.GenericContainer(
+//		context.Background(),
+//		testcontainers.GenericContainerRequest{
+//			ContainerRequest: containerReq,
+//			Started:          true,
+//		})
+//
+//	host, _ := dbContainer.Host(context.Background())
+//	port, _ := dbContainer.MappedPort(context.Background(), "5432")
+//
+//	dsn := fmt.Sprintf("host=%s port=%d user=postgres password=postgres dbname=testdb sslmode=disable", host, port.Int())
+//	pureDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+//	if err != nil {
+//		return nil, "", 0, fmt.Errorf("gorm open: %w", err)
+//	}
+//
+//	sqlDB, err := pureDB.DB()
+//	if err != nil {
+//		return nil, "", 0, fmt.Errorf("get db: %w", err)
+//	}
+//
+//	if err = goose.Up(sqlDB, "../../deployments/migrations/migrations_postgres"); err != nil {
+//		return nil, "", 0, fmt.Errorf("up migrations: %w", err)
+//	}
+//
+//	return dbContainer, host, port.Int(), nil
+//}
 
-	dbContainer, _ := testcontainers.GenericContainer(
-		context.Background(),
-		testcontainers.GenericContainerRequest{
-			ContainerRequest: containerReq,
-			Started:          true,
-		})
-
-	host, _ := dbContainer.Host(context.Background())
-	port, _ := dbContainer.MappedPort(context.Background(), "5432")
-
-	dsn := fmt.Sprintf("host=%s port=%d user=postgres password=postgres dbname=testdb sslmode=disable", host, port.Int())
-	pureDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, "", 0, fmt.Errorf("gorm open: %w", err)
-	}
-
-	sqlDB, err := pureDB.DB()
-	if err != nil {
-		return nil, "", 0, fmt.Errorf("get db: %w", err)
-	}
-
-	if err = goose.Up(sqlDB, "../../deployments/migrations/migrations_postgres"); err != nil {
-		return nil, "", 0, fmt.Errorf("up migrations: %w", err)
-	}
-
-	return dbContainer, host, port.Int(), nil
-}
-
-type dbConfig struct {
-	Host string
-	Port int
-}
+//type dbConfig struct {
+//	Host string
+//	Port int
+//}
 
 func TestMain(m *testing.M) {
 	tmplFile := flag.String("tmpl", "../../configs/test_config_template.yml", "tmpl file name")
